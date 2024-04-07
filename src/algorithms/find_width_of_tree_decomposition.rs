cdbf13dbeef4bafc0470521538198eb162aa7705
RaoulLuque <raoulsluque@gmail.com>
2024-04-07 14:09:02 +0200

    Fixed typo

--- src/algorithms/find_width_of_tree_decomposition.rs
+++ src/algorithms/find_width_of_tree_decomposition.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 
 /// Returns the maximum size of one of the bags in the tree decomposition graph.
 /// This equals the highest len of one of the vertices in the graph. Returns 0 if the graph has no vertices
-pub fn find_width_of_tree_decompositionpub<E>(
+pub fn find_width_of_tree_decomposition<E>(
     graph: Graph<HashSet<NodeIndex>, E, petgraph::prelude::Undirected>,
 ) -> usize {
     if let Some(bag) = graph.node_weights().max_by_key(|b| b.len()) {
