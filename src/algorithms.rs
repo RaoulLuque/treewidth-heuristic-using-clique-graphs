85ca3fc56578d8cb107ed48d04258bf029d8aa6a
RaoulLuque <raoulsluque@gmail.com>
2024-04-07 14:08:29 +0200

    Implemented function to calculate treewidth given graph with HashSets as vertex weights

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -2,3 +2,4 @@ pub mod construct_clique_graph;
 pub mod fill_bags_along_path;
 pub mod find_maximum_cliques;
 pub mod find_path_in_tree;
+pub mod find_width_of_tree_decomposition;
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use petgraph::Graph;
 
 fn main() {
     let mut graph: Graph<u32, u32, petgraph::prelude::Undirected> =
-            petgraph::Graph::new_undirected();
+        petgraph::Graph::new_undirected();
 
     let nodes = [
         graph.add_node(0),
@@ -29,6 +29,4 @@ fn main() {
     graph.add_edge(nodes[4], nodes[6], 0);
 
     let mut cliques: Vec<Vec<_>> = find_maximum_cliques::<Vec<_>, _>(&graph).collect();
-
-        
 }
