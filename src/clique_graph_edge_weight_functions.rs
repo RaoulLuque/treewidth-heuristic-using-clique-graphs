0be6b953fd21db7c7092e63a114bc6cf51ff50d0
Raoul Luqué <raoulsluque@gmail.com>
2024-06-22 19:08:08 +0200

    Rename edge weight heuristics to edge weight functions

--- src/clique_graph_edge_weight_functions.rs
+++ src/clique_graph_edge_weight_functions.rs
@@ -6,7 +6,7 @@ pub fn neutral_heuristic<S>(_: &HashSet<NodeIndex, S>, _: &HashSet<NodeIndex, S>
     0
 }
 
-pub fn negative_intersection_heuristic<S: BuildHasher + Default>(
+pub fn negative_intersection<S: BuildHasher + Default>(
     first_vertex: &HashSet<NodeIndex, S>,
     second_vertex: &HashSet<NodeIndex, S>,
 ) -> i32 {
@@ -16,7 +16,7 @@ pub fn negative_intersection_heuristic<S: BuildHasher + Default>(
         .len() as i32)
 }
 
-pub fn positive_intersection_heuristic<S: BuildHasher + Default>(
+pub fn positive_intersection<S: BuildHasher + Default>(
     first_vertex: &HashSet<NodeIndex, S>,
     second_vertex: &HashSet<NodeIndex, S>,
 ) -> i32 {
@@ -26,14 +26,14 @@ pub fn positive_intersection_heuristic<S: BuildHasher + Default>(
         .len() as i32
 }
 
-pub fn disjoint_union_heuristic<S: BuildHasher>(
+pub fn disjoint_union<S: BuildHasher>(
     first_vertex: &HashSet<NodeIndex, S>,
     second_vertex: &HashSet<NodeIndex, S>,
 ) -> i32 {
     (first_vertex.len() + second_vertex.len()) as i32
 }
 
-pub fn union_heuristic<S: BuildHasher + Default>(
+pub fn union<S: BuildHasher + Default>(
     first_vertex: &HashSet<NodeIndex, S>,
     second_vertex: &HashSet<NodeIndex, S>,
 ) -> i32 {
@@ -43,7 +43,7 @@ pub fn union_heuristic<S: BuildHasher + Default>(
         .len() as i32
 }
 
-pub fn least_difference_heuristic<S: BuildHasher + Default>(
+pub fn least_difference<S: BuildHasher + Default>(
     first_vertex: &HashSet<NodeIndex, S>,
     second_vertex: &HashSet<NodeIndex, S>,
 ) -> i32 {
@@ -53,22 +53,22 @@ pub fn least_difference_heuristic<S: BuildHasher + Default>(
         .len() as i32
 }
 
-pub fn negative_intersection_then_least_difference_heuristic<S: BuildHasher + Default>(
+pub fn negative_intersection_then_least_difference<S: BuildHasher + Default>(
     first_vertex: &HashSet<NodeIndex, S>,
     second_vertex: &HashSet<NodeIndex, S>,
 ) -> (i32, i32) {
     (
-        negative_intersection_heuristic(first_vertex, second_vertex),
-        least_difference_heuristic(first_vertex, second_vertex),
+        negative_intersection(first_vertex, second_vertex),
+        least_difference(first_vertex, second_vertex),
     )
 }
 
-pub fn least_difference_then_negative_intersection_heuristic<S: BuildHasher + Default>(
+pub fn least_difference_then_negative_intersection<S: BuildHasher + Default>(
     first_vertex: &HashSet<NodeIndex, S>,
     second_vertex: &HashSet<NodeIndex, S>,
 ) -> (i32, i32) {
     (
-        least_difference_heuristic(first_vertex, second_vertex),
-        negative_intersection_heuristic(first_vertex, second_vertex),
+        least_difference(first_vertex, second_vertex),
+        negative_intersection(first_vertex, second_vertex),
     )
 }
